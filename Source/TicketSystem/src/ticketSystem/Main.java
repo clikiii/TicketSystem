5dc7045c68867e13f166b547ee9867fdce95cf92
clikiii <18019349217@163.com>
2022-10-22 15:41:03 +0800

    feat: user class

--- Source/TicketSystem/src/ticketSystem/Main.java
+++ Source/TicketSystem/src/ticketSystem/Main.java
@@ -7,11 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // System.out.println("Hello world!");
         Database db = new Database();
-        UserDAO userDAO = new UserDAO();
-        // userDAO.addUser(db, "BBB", "123");
-        // userDAO.changePwd(db, "AAA", "456");
-        userDAO.queryUser(db, "BBB", "123");
-        // userDAO.deleteUser(db, "AAA", "456");
+        People currentUser = new User().register(db, "CCC", "789");
         db.closeConn();
     }
 }
